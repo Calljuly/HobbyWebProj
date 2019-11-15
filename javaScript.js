67e5b3730a8fa7505e8e9a48a79481d9f3bca4d1
Lydia Nilsson <lydia.nilsson@gmail.com>
2019-11-15 14:54:52 +0100

    Added a function for changing fonts and also added a function to convert our date representation of "senast uppdaterad"

--- javaScript.js
+++ javaScript.js
@@ -1,7 +1,21 @@
 function getTimeForLastModified()
 {
-    document.getElementById("content2").innerHTML = "Last updated : "+ document.lastModified;
+    var date = new Date(document.lastModified);
+    var dateToPrint = getFormattedDate(date);
+    document.getElementById("content2").innerHTML = "Senast uppdaterad: " + dateToPrint;
 }
+
+function getFormattedDate(date) {
+    var year = date.getFullYear();
+  
+    var month = (1 + date.getMonth()).toString();
+    month = month.length > 1 ? month : '0' + month;
+  
+    var day = date.getDate().toString();
+    day = day.length > 1 ? day : '0' + day;
+    
+    return day + '/' + month  + '/' + year;
+  }
 
 function myTimer(){
     var sec = 1;
@@ -16,4 +30,22 @@ function myTimer(){
         }
         
     } ,1000);
+}
+
+function changeFont(){
+    if(document.body.style.fontFamily === "Arial"){
+        document.body.style.fontFamily = "Open Sans Condensed";
+        var buttonText = document.getElementsByClassName("buttontext");
+        for(i = 0; i < buttonText.length; i++){
+            buttonText[i].style.fontFamily = "Open Sans Condensed";
+        }
+    }
+    else{
+        document.body.style.fontFamily = "Arial";
+        var buttonText = document.getElementsByClassName("buttontext");
+        for(i = 0; i < buttonText.length; i++){
+            buttonText[i].style.fontFamily = "Arial";
+        }
+    }
+
 }
